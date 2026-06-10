83324da1955f587bf130827e5671a1e0fb921733
Rayaine <mechernanerk@gmail.com>
2026-06-10 19:12:47 +0100

    Add: string-encode-and-decode - submission-15

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-15.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder encodedString = new StringBuilder();
+        for( String str : strs ) {
+            encodedString.append( str.length() + "/" + str );
+        }
+        return encodedString.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> decodedStrs = new ArrayList<>();
+        String string;
+        int i = 0;
+        int stringFinalChar;
+        int slashIndex;
+        int stringSize;
+        while ( i < str.length() ) {
+            slashIndex = str.indexOf('/',i);
+            stringSize = Integer.parseInt(str.substring(i,slashIndex));
+            i = slashIndex + 1;
+            stringFinalChar = stringSize  + slashIndex;
+            string = str.substring(i,stringFinalChar+1);
+            i = stringFinalChar + 1;
+            decodedStrs.add(string);
+        }
+        return decodedStrs;
+    }
+}
