c3b594d35faae5136434d9c589ff2de670d7c8e8
Rayaine <mechernanerk@gmail.com>
2026-07-23 21:05:27 +0100

    Add: eating-bananas - submission-9

---
+++ Data Structures & Algorithms/eating-bananas/submission-9.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int upperBound = piles[0];
+        for( int i = 1 ; i < piles.length ; i++ ){
+            if( upperBound < piles[i] ) upperBound = piles[i];
+        }
+        int lowerBound = 1;
+        int possiblePace = 0;
+        int validPace = 0;
+        while( lowerBound <= upperBound ){
+            int neededHours = 0;
+            possiblePace = (lowerBound+upperBound)/2;
+            for(  int i = 0 ; i < piles.length ; i++  ) {
+                neededHours += (piles[i] + possiblePace - 1) / possiblePace;
+                if( neededHours > h ) {
+                    lowerBound = possiblePace+1;
+                    break;
+                }
+            }
+            if( neededHours <= h ) {
+                validPace = possiblePace;
+                upperBound = possiblePace-1;
+            }
+
+        }
+        return validPace;
+    }
+}
